ad529b58c6ae9cd568211d2f0b89b24664333af0
Ivica Jangelovski <moove@Mooves-MacBook-Pro.local>
2018-03-12 13:44:24 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Datalon</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="css/agency.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" style="margin-top: 50px;">
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Datalon</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#about">Timeline</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#victims-data-table">Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#team">Jews Of Bitola</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#portfolio">Gallery</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Header -->
+<header class="masthead">
+    <div class="container">
+        <div class="intro-text">
+            <div class="intro-heading text-uppercase">The March Of The Living from Bitola</div>
+            <a class="text-uppercase js-scroll-trigger" href="#about"><img src="img/arrow-green.png"/></a>
+        </div>
+    </div>
+</header>
+
+<!-- About -->
+<section id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading text-uppercase">Timeline</h2>
+                <h3 class="section-subheading text-muted"></h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image" style="background-image:url('img/01.jpg')">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="subheading">Monastir (Bitola, Macedonia)</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">"An ancient Jewish town with no Jews – the fires of Treblinka consumed them all..."
+                                    Shlomo Alboher</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image" style="background-image:url('img/02.jpg')">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Monastir Community</h4>
+                                <h4 class="subheading"></h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">Story of a community of many years and a multitude of virtues, a community that did not allow poverty or other troubles to disturb its Jewish traditions, the story of a community that was almost completely destroyed.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="timeline-image" style="background-image:url('img/03.jpg')">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Deportation of the Jews of Macedonia, March 1943</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">On 6 April 1941, Nazi Germany attacked Yugoslavia. By the next day, German tanks had entered Skopje, the capital of Macedonia, and on 9 April Monastir (Bitola) was occupied. For three days straight, the Germans looted Jewish-owned shops across Macedonia, including Monastir. This was accompanied by violent acts of oppression and humiliation. A number of people were killed.
+
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image" style="background-image:url('img/04.jpg')">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Commemoration of the Monastir Community</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">“Today there isn’t a single Jew in Monastir, where one hears no more the voice of the songs and of the ballads, nor the voices of mothers and of prayers. The synagogues that were left were like mountains of rubble. There remained only the cemetery and the ruins.”
+                                    - Jamila Andjela Kolonomos</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>Their <br> Legacies<br> Remain...</h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="victims-data-table" class="victims-data-table">
+    <div class="container">
+        <h1 class="text-center" style="margin-top: 30px;">Data visualization</h1>
+        <iframe width="100%" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiMmZkNDViMTctMGZhZC00YzlhLWE2ODUtYTU2YmQ4YTg0NmQzIiwidCI6ImYwY2FjNDdiLWUyYjMtNGUxYi1hNTJmLTQ4N2QyZDk5NjI4OCIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
+        <br/>
+        <br/>
+
+        <h1 class="text-center" style="margin-bottom: 30px;">Search victims</h1>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>YV ID</th>
+                <th>Full Name</th>
+                <th>From</th>
+                <th>Birth year</th>
+                <th>Death date</th>
+                <th>Country death</th>
+            </tr>
+            </thead>
+
+            <tfoot>
+            <tr>
+                <th>ID</th>
+                <th>YV ID</th>
+                <th>Full Name</th>
+                <th>From</th>
+                <th>Birth year</th>
+                <th>Death date</th>
+                <th>Country death</th>
+            </tr>
+            </tfoot>
+
+            <tbody>
+            @foreach($victims as $key => $victim)
+                <tr>
+                    <td>{{ $victim->id }}</td>
+                    <td>{{ $victim->BOOK_ID }}</td>
+                    <td><a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">{{ $victim->FIRST_NAME . ' ' . $victim->LAST_NAME }}</a></td>
+                    <td>{{ $victim->PLACE }}</td>
+                    <td class="{{ $victim->YEAR_BIRTH != '0' ? '' : 'no-data' }}">
+                        {{ $victim->YEAR_BIRTH != '0' ? $victim->YEAR_BIRTH : 'Not provided' }}
+                    </td>
+                    <td class="{{ $victim->YEAR_DEATH != '0' ? '' : 'no-data' }}">
+                        {{ $victim->YEAR_DEATH != '0' ? $victim->YEAR_DEATH : 'Not provided'}}
+                    </td>
+                    <td class="{{ $victim->COUNTRY_DEATH != 'Unknown country' ? '' : 'no-data' }}">
+                        {{ $victim->COUNTRY_DEATH }}
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+
+        </table>
+    </div>
+</section>
+
+<script>
+    $ = jQuery;
+    $(document).ready(function () {
+        $('.victims-data-table table').DataTable({
+            "pagingType": "full_numbers"
+        });
+    })
+</script>
+
+
+<!-- Team -->
+<section class="bg-light" id="team">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading text-uppercase">Jews Of Bitola</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/Isak_Koen.jpeg" alt="">
+                    <h4>Isak Koen</h4>
+                    <p class="text-muted">Portrait of Isak Koen. He lived on Asadbegova in Bitola.</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/alkuser.jpeg" alt="">
+                    <h4>Portrait of Schua Alkuser.</h4>
+                    <p class="text-muted">Portrait of Schua Alkuser.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/MairAlkuser.jpeg" alt="">
+                    <h4>Mair Alkuser</h4>
+                    <p class="text-muted">Portrait of Mair Alkuser. He was a tailor. He lived at Dalmatinska 72 in Bitola.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Services -->
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading text-uppercase">Dr. Haim Abravanel</h2>
+                <h3 class="section-subheading text-muted">The only doctor in Bitola after the Jews were deported.</h3>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-4">
+                <div class="timeline-image" style="background-image:url('img/375px-Haim_Abravanel.jpg')">
+                </div>
+                <p class="text-muted">Dr. pediatrician Haim Abravanel with his family  was the onlyone  freed family from the Bulgarian occupying forces from the gathered Bitola Jews, due to the need of doctors in Bitola.</p>
+            </div>
+            <div class="col-md-4">
+                <div class="timeline-image" style="background-image:url('img/image1.jpeg')">
+                </div>
+                <p class="text-muted">During the occupation, the only Jewish doctors' families took care of their fellow citizens from Bitola.
+                    Dr. Abrahaman and his wife, Bertha, moved to Israel in the late 50's.</p>
+            </div>
+            <div class="col-md-4">
+                <div class="timeline-image" style="background-image:url('img/image3.jpeg')">
+                </div>
+                <p class="text-muted">Today, the Health Center-Bitola is named after Dr. Abravanel.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Portfolio Grid -->
+<section class="bg-light" id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading text-uppercase">Gallery</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                    </div>
+                </div>
+                <img class="img-fluid" src="img/FCP_3520.jpg" alt="">
+                <div class="portfolio-caption">
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                    </div>
+                </div>
+                <img class="img-fluid" src="img/FCP_3543.jpg" alt="">
+                <div class="portfolio-caption">
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                    </div>
+                </div>
+                <img class="img-fluid" src="img/FCP_3600.jpg" alt="">
+                <div class="portfolio-caption">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <span class="copyright">Copyright &copy; Datalon 2018 - Powered by Team12</span>
+            </div>
+            <div class="col-md-4">
+                <ul class="list-inline social-buttons">
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fa fa-linkedin"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Portfolio Modals -->
+
+<!-- Modal 1 -->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 mx-auto">
+                        <div class="modal-body row">
+                            <div class="col-lg-4">
+                                <img alt="Portrait of Palomba Aleshandra, wife of Avram Aleshandra.  She lived at Belika Avliya 3 in Bitola." src="img/team/ISAK.jpeg" style="max-width:100%; max-height:400px;" title="Portrait of Isak, wife of Avram Aleshandra.">
+                            </div>
+                            <div class="col-lg-8">
+                                <h2 class="text-uppercase text-left">Isak Albakares</h2>
+                                <p class="item-intro text-muted text-left">1917 - 1943</p>
+                                <p class="text-left">
+                                    Portrait of Isak Albaranes. He lived at Karagoryeva 77 in Bitola.
+                                </p>
+                                <ul class="text-left">
+                                    <li>Place Of Birth: Bitola, Macedonia</li>
+                                    <li>Residence Country: Macedonia</li>
+                                    <li>Place Of Death: Camp, Poland</li>
+                                </ul>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27851.544073919056!2d21.30864809539435!3d41.028821470914586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13572451365fb6fd%3A0xfa67bbbf65bec948!2sBitola!5e0!3m2!1sen!2smk!4v1520758457094" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                <button class="btn btn-primary" data-dismiss="modal" type="button" style="margin-top: 20px; min-width: 160px;">
+                                    <i class="fa fa-times"></i>
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Contact form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/agency.min.js"></script>
+<script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+</body>
+
+</html>
